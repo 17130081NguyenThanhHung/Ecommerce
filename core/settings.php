bbe2a7a3a9b6c7084b1abc565c6a4732e1af9ca3
Nguyễn Thanh Hưng <55803563+17130081NguyenThanhHung@users.noreply.github.com>
2020-12-23 22:45:36 +0700

    Update settings.php
    config db

--- core/settings.php
+++ core/settings.php
@@ -1,23 +1,23 @@
 <?php
  
-# MySQL Host
-$sql_db_host = "localhost";  // mặc định là localhost
-# MySQL Tài khoản
-$sql_db_user = "root";
-# MySQL Mật khẩu
-$sql_db_pass = "000000";
-# MySQL Data
-$sql_db_name = "e-phones";
-#  Chỉnh sửa URL website của bạn
-$site_url    = "https://localhost/Ephones"; // E.g (http://example.com)
-// <?php
 // # MySQL Host
-// $sql_db_host = "qn0cquuabmqczee2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  // mặc định là localhost
+// $sql_db_host = "localhost";  // mặc định là localhost
 // # MySQL Tài khoản
-// $sql_db_user = "e81mid05pbpjzspq";
+// $sql_db_user = "root";
 // # MySQL Mật khẩu
-// $sql_db_pass = "m0af8zxczutx79ti";
+// $sql_db_pass = "000000";
 // # MySQL Data
-// $sql_db_name = "djbw8sjtovojdy9w";
+// $sql_db_name = "e-phones";
 // #  Chỉnh sửa URL website của bạn
-// $site_url    = "https://web-dientu.herokuapp.com/"; // E.g (http://example.com)
+// $site_url    = "https://localhost/Ephones"; // E.g (http://example.com)
+<?php
+# MySQL Host
+$sql_db_host = "jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  // mặc định là localhost
+# MySQL Tài khoản
+$sql_db_user = "kvpn3kzoetmca0tu";
+# MySQL Mật khẩu
+$sql_db_pass = "kbdkj4unjyxc5oou";
+# MySQL Data
+$sql_db_name = "vda0hmmm8uy73crj";
+#  Chỉnh sửa URL website của bạn
+$site_url    = "https://ephones.herokuapp.com/"; // E.g (http://example.com)
